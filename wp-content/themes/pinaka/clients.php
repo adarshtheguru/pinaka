bd0c43e073f15e1fcbf42112531fcb9e51f80095
Adarsh <adarshji1999@gmail.com>
2025-03-31 09:26:31 +0530

    client cmp

--- wp-content/themes/pinaka/clients.php
+++ wp-content/themes/pinaka/clients.php
@@ -18,31 +18,127 @@ get_header();
                             Introduce the concept of partnership. Highlight how PINAKA collaborates with clients to tailor solutions that address their specific challenges in the real estate marketing space.
                         </p>
                     </div>
-                    <div class="gridBox">
+                    <div class="partB">
+                        <div class="head">
+                            <div class="title">Our Clients</div>
+                            <div class="dropdown">
+                                <select name="chooseIndustry" id="chooseIndustry">
+                                    <option value="">Choose industry</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="gridBox">
+                            <div class="box">
+                                <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/shapoorji.webp" alt="">
+                            </div>
+                            <div class="box">
+                                <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/client2.webp" alt="">
+                            </div>
+                            <div class="box">
+                                <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/client3.webp" alt="">
+                            </div>
+                            <div class="box">
+                                <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/client4.webp" alt="">
+                            </div>
+                            <div class="box">
+                                <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/client5.webp" alt="">
+                            </div>
+                            <div class="box">
+                                <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/client6.webp" alt="">
+                            </div>
+                            <div class="box">
+                                <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/client7.webp" alt="">
+                            </div>
+                            <div class="box">
+                                <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/client8.webp" alt="">
+                            </div>
+                            <div class="box">
+                                <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/client9.webp" alt="">
+                            </div>
+                            <div class="box">
+                                <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/client10.webp" alt="">
+                            </div>
+                            <div class="box">
+                                <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/client11.webp" alt="">
+                            </div>
+                            <div class="box">
+                                <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/client12.webp" alt="">
+                            </div>
+                            <div class="box">
+                                <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/client13.webp" alt="">
+                            </div>
+                            <div class="box">
+                                <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/client14.webp" alt="">
+                            </div>
+                            <div class="box">
+                                <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/client15.webp" alt="">
+                            </div>
+                            <div class="box">
+                                <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/client16.webp" alt="">
+                            </div>
+                        </div>
+                        <div class="text-center">
+                            <a href="javascript:;" class="blankBtn">LOAD MORE</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="padding buisSol" id="buisSol">
+            <div class="container">
+                <div class="text-center secTitle">Business Solutions, Reimagined</div>
+                <div class="buisSolBox">
+                    <div class="box">
+                        <img src="<?php echo THEMEURL; ?>/assets/images/targetTick.svg" alt="">
+                        <div class="title">Customized Strategies for Growth</div>
+                        <p class="para">Tailored marketing approaches to showcase unique offerings and amplify brand impact.</p>
+                    </div>
+                    <div class="box">
+                        <img src="<?php echo THEMEURL; ?>/assets/images/speaker.svg" alt="">
+                        <div class="title">Visibility and Engagement Boost</div>
+                        <p class="para">Innovative digital solutions to enhance brand visibility and drive customer engagement.</p>
+                    </div>
+                    <div class="box">
+                        <img src="<?php echo THEMEURL; ?>/assets/images/greenBar.svg" alt="">
+                        <div class="title">Data-Driven Decision-Making</div>
+                        <p class="para">Leveraging advanced analytics and insights to support smarter business decisions.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="padding ambitions" id="ambitions">
+            <div class="container">
+                <div class="partA text-center">
+                    <div class="secTitle">Transforming Ambitions into Achievements</div>
+                    <div class="para">At PINAKA, our focus is on measurable results that drive your business forward. From increasing brand reach to enhancing lead quality, our tailored solutions bring success stories to life."</div>
+                </div>
+                <div class="partB">
+                    <div class="dataBox">
                         <div class="box">
-                            <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/shapoorji.webp" alt="">
+                            <div class="number">60%</div>
+                            <div class="title">Boost in Lead Quality</div>
+                            <p class="para">
+                                Our data-centric approach raised lead quality by 60%, helping clients attract and engage the right audience effectively.
+                            </p>
                         </div>
                         <div class="box">
-                            <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/shapoorji.webp" alt="">
+                            <div class="number">120%</div>
+                            <div class="title">Increase in Organic Traffic</div>
+                            <p class="para">
+                                With our SEO strategies, clients have seen a 120% increase in organic site traffic, bringing long-term growth and visibility.
+                            </p>
                         </div>
                         <div class="box">
-                            <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/shapoorji.webp" alt="">
+                            <div class="number">40%</div>
+                            <div class="title">Reduction in Cost-Per-Lead</div>
+                            <p class="para">
+                                We helped clients cut lead generation costs by 40%, allowing for more budget to go toward growth and innovation.
+                            </p>
                         </div>
-                        <div class="box">
-                            <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/shapoorji.webp" alt="">
-                        </div>
-                        <div class="box">
-                            <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/shapoorji.webp" alt="">
-                        </div>
-                        <div class="box">
-                            <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/shapoorji.webp" alt="">
-                        </div>
-                        <div class="box">
-                            <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/shapoorji.webp" alt="">
-                        </div>
-                        <div class="box">
-                            <img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/shapoorji.webp" alt="">
-                        </div>
+                    </div>
+                    <div class="text-center">
+                        <a href="jaavscript:;" class="themeBtn">Schedule a Free Consultation</a>
                     </div>
                 </div>
             </div>
