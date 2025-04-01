a84219b554eec63671d59f6397a57c28911828bc
Pragnesh <pragnesh@gmail.com>
2025-04-01 13:59:20 +0530

    Add service page ppc

---
+++ wp-content/themes/pinaka/service-ppc.php
@@ -0,0 +1,176 @@
+<?php
+/**
+ * Template Name: Service PPC
+ */
+get_header();
+?>
+
+<section class="padding brandHero h100 basicBg" id="brandHero">
+    <div class="container">
+		<div class="bannerCont">
+			<div class="content">
+				<div class="mainTitle">Lorem Ipsum is  dummy text of the printing industry.</div>
+				<p class="para">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+				<div class="ctas">
+					<a href="javascript:;" class="themeBtn">Explore More</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="scrollDownWrapper">
+		<div class="pos-rel">
+			<img class="img-responsive" src="<?php echo THEMEURL; ?>/assets/images/scrollPatch.svg" alt="">
+			<img src="<?php echo THEMEURL; ?>/assets/images/arrowBot.svg" alt="" class="sdArrow img-responsive">
+		</div>
+	</div>	
+</section>
+
+<section class="padding branding services-brand" id="branding">
+    <div class="container">
+        <div class="secTitle">PPC</div>
+        <div class="brandingCont">
+            <div class="sideBar">
+                <div class="box">
+                    <div>About Us</div>
+                    <div>Our Services</div>
+                    <div>Why choose us</div>
+                    <div>You get more</div>
+                    <div>Our Customers Say</div>
+                </div>
+            </div>
+            <div class="mainCont">
+               <div class="aboutUs">
+                    <div class="secPrefix">About Us</div>
+                    <div class="secTitle">
+                       In the fast-paced digital landscape, businesses need data-driven strategies to generate high-quality leads and maximize ROI.
+                    </div>
+                    <p class="para">
+                        At Pinaka Digital, we specialize in Paid Marketing and Performance Marketing, helping brands achieve measurable results through targeted campaigns. Our expertise in PPC, Facebook Marketing, Google Ads Marketing, LinkedIn Marketing, Bing Ads, and conversion optimization ensures that your business reaches the right audience at the right time, driving higher engagement and increased revenue.<br>
+                        With a deep understanding of market trends and consumer behavior, we craft performance-focused campaigns that deliver real, tangible outcomes. Whether you are looking to boost brand awareness, drive website traffic, or increase sales, our tailored solutions will help you achieve your business goals effectively.
+                    </p>
+                    <section class="padding">
+                    <div class="secPrefix">Our Services Include</div>
+                    <div class="secTitle">
+                       Lorem Ipsum is simply dummy.
+                    </div>
+                    <div class="services-list-wrap">
+                        <div class="inner-service-list">
+                            <div class="service-item">
+                                <div class="service-icon">
+                                    <img src="<?php echo THEMEURL; ?>/assets/images/service-icon.svg">
+                                </div>
+                                <div class="service-info">
+                                    <h3 class="service-title">Pay-Per-Click (PPC) Advertising</h3>
+                                    <p class="service-desc">Google Ads Marketing, Bing Ads, and Display Advertising</p>
+                                </div>
+                                <div class="service-btn">
+                                    <a href="javascript:;">ENQUIRE NOW</a>
+                                </div>
+                            </div>
+                            <div class="service-item">
+                                <div class="service-icon">
+                                    <img src="<?php echo THEMEURL; ?>/assets/images/service-icon.svg">
+                                </div>
+                                <div class="service-info">
+                                    <h3 class="service-title">Social Media Advertising</h3>
+                                    <p class="service-desc">Facebook Marketing, Instagram, LinkedIn Marketing & Twitter Ads</p>
+                                </div>
+                                <div class="service-btn">
+                                    <a href="javascript:;">ENQUIRE NOW</a>
+                                </div>
+                            </div>
+                            <div class="service-item">
+                                <div class="service-icon">
+                                    <img src="<?php echo THEMEURL; ?>/assets/images/service-icon.svg">
+                                </div>
+                                <div class="service-info">
+                                    <h3 class="service-title">E-commerce Advertising</h3>
+                                    <p class="service-desc">Google Shopping, Amazon Ads, and Marketplace Promotions</p>
+                                </div>
+                                <div class="service-btn">
+                                    <a href="javascript:;">ENQUIRE NOW</a>
+                                </div>
+                            </div>
+                            <div class="service-item">
+                                <div class="service-icon">
+                                    <img src="<?php echo THEMEURL; ?>/assets/images/service-icon.svg">
+                                </div>
+                                <div class="service-info">
+                                    <h3 class="service-title">Remarketing & Retargeting</h3>
+                                    <p class="service-desc">Engaging past visitors to boost conversions through Google Ads Marketing and Facebook Marketing.</p>
+                                </div>
+                                <div class="service-btn">
+                                    <a href="javascript:;">ENQUIRE NOW</a>
+                                </div>
+                            </div>
+                            <div class="service-item">
+                                <div class="service-icon">
+                                    <img src="<?php echo THEMEURL; ?>/assets/images/service-icon.svg">
+                                </div>
+                                <div class="service-info">
+                                    <h3 class="service-title">Performance Analytics & Optimization</h3>
+                                    <p class="service-desc">Data-driven campaign enhancements for Paid Marketing success.</p>
+                                </div>
+                                <div class="service-btn">
+                                    <a href="javascript:;">ENQUIRE NOW</a>
+                                </div>
+                            </div>
+                            <div class="service-item">
+                                <div class="service-icon">
+                                    <img src="<?php echo THEMEURL; ?>/assets/images/service-icon.svg">
+                                </div>
+                                <div class="service-info">
+                                    <h3 class="service-title">Lead Generation Campaigns</h3>
+                                    <p class="service-desc">High-intent lead capture for better ROI through Google Ads, Facebook Marketing, and LinkedIn Marketing.</p>
+                                </div>
+                                <div class="service-btn">
+                                    <a href="javascript:;">ENQUIRE NOW</a>
+                                </div>
+                            </div>
+                        </div>
+                        
+
+
+                    </div>
+                </section>
+                <section class="padding pd-tp-5">
+                    <div class="secPrefix">Paid Marketing</div>
+                    <div class="secTitle">
+                       Why Choose Us for Paid Marketing?
+                    </div>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard since the 1500s.</p>
+                </section>
+                    <div class="secTitle">
+                       You Get More with Pinaka Digital
+                    </div>
+                    <div class="abtBoxes">
+                        <div class="box">
+                            <div class="number">100+</div>
+                            <div class="title">Developers</div>
+                            <p>A robust tech team supporting your marketing initiatives for Paid Marketing.</p>
+                        </div>
+                        <div class="box">
+                            <div class="number">500+</div>
+                            <div class="title">Projects</div>
+                            <p>Proven expertise across diverse industries in Facebook Marketing, Google Ads Marketing, & LinkedIn Marketing.</p>
+                        </div>
+                        <div class="box">
+                            <div class="number">2000+</div>
+                            <div class="title">Leads Per Day</div>
+                            <p>High-quality leads generated consistently through Paid Marketing strategies, including Bing and Taboola campaigns.</p>
+                        </div>
+                    </div>
+
+                    <div class="secPrefix">Testimonials</div>
+                    <div class="secTitle">
+                       Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    </div>
+                    <div class="">
+               </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include 'customTemplates/footerGreen.php'; ?>
+
+<?php get_footer(); ?>
