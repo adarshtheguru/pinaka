d0614b4135586362234976f76d4c9e424048adc2
pragneshui <pragneshlimbasiya@gmail.com>
2025-09-18 02:46:38 +0530

    resposive home page section

--- wp-content/themes/pinaka/customTemplates/serviceDefault.php
+++ wp-content/themes/pinaka/customTemplates/serviceDefault.php
@@ -205,51 +205,53 @@
 		</div>
 	</div>
 </section>
-<section id="caseStudies" class="padding caseStudies">
-	<div class="container">
-		<div class="partA">
-			<div class="title">Relevant Case Studies</div>
-			<a href="<?php echo get_post_type_archive_link('case_study'); ?>" class="themeBtn">See All CASE STUDIES</a>
-		</div>
-	</div>
-	<div class="case-study-wrapper">
-        <h2><?php echo ucfirst($service_slug); ?> Case Studies</h2>
-        <div class="case-study-slider">
-            <?php
-            $args = [
-                'post_type'      => 'case_study',
-                'posts_per_page' => -1,
-                'post_status'    => 'publish',
-                'tax_query'      => [
-                    [
-                        'taxonomy' => 'case_study_category',
-                        'field'    => 'slug',
-                        'terms'    => $service_slug, // match service slug to category slug
-                    ],
-                ],
-            ];
-            $query = new WP_Query($args);
+<?php
+$service_slug = get_post_field('post_name', get_the_ID());
 
-            if ($query->have_posts()) :
-                while ($query->have_posts()) : $query->the_post();
-                    $image_id  = get_field('service_thumbnail_image', get_the_ID());
-                    $image_url = wp_get_attachment_image_url($image_id, 'full');
-            ?>
-                <div class="case-study-item">
-                    <a href="<?php the_permalink(); ?>">
-                        <?php if ($image_url): ?>
-                            <img src="<?php echo esc_url($image_url); ?>" alt="<?php the_title_attribute(); ?>" />
-                        <?php endif; ?>
-                        <p class="title"><?php the_title(); ?></p>
-                    </a>
-                </div>
-            <?php
-                endwhile;
-                wp_reset_postdata();
-            else :
-                echo '<p>No case studies found for this service.</p>';
-            endif;
-            ?>
+// Query case studies for this service
+$args = [
+    'post_type'      => 'case_study',
+    'posts_per_page' => -1,
+    'post_status'    => 'publish',
+    'tax_query'      => [
+        [
+            'taxonomy' => 'case_study_category',
+            'field'    => 'slug',
+            'terms'    => $service_slug,
+        ],
+    ],
+];
+$query = new WP_Query($args);
+
+if ($query->have_posts()) : ?>
+    <section id="caseStudies" class="padding caseStudies">
+        <div class="container">
+            <div class="partA">
+                <div class="title">Relevant Case Studies</div>
+                <a href="<?php echo get_post_type_archive_link('case_study'); ?>" class="themeBtn">See All CASE STUDIES</a>
+            </div>
         </div>
-    </div>
-</section>
+
+        <div class="case-study-wrapper">
+            <h2><?php echo ucfirst($service_slug); ?> Case Studies</h2>
+            <div class="case-study-slider">
+                <?php while ($query->have_posts()) : $query->the_post();
+                    $image_id  = get_field('service_thumbnail_image', get_the_ID());
+                    $image_url = wp_get_attachment_image_url($image_id, 'full'); ?>
+                    
+                    <div class="case-study-item">
+                        <a href="<?php the_permalink(); ?>">
+                            <?php if ($image_url): ?>
+                                <img src="<?php echo esc_url($image_url); ?>" alt="<?php the_title_attribute(); ?>" />
+                            <?php endif; ?>
+                            <p class="title"><?php the_title(); ?></p>
+                        </a>
+                    </div>
+                <?php endwhile; ?>
+            </div>
+        </div>
+    </section>
+<?php
+endif;
+wp_reset_postdata();
+?>
