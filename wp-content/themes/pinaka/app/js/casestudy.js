9b45a555bfe78d2a52a777b4d7165774123882d2
pragneshui <pragneshlimbasiya@gmail.com>
2025-09-20 01:02:55 +0530

    Home page changes

---
+++ wp-content/themes/pinaka/app/js/casestudy.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function ($) {
+    $('#caseStudyFilter').on('change', function () {
+        var category = $(this).val();
+
+        $.ajax({
+            url: caseStudyAjax.ajaxurl, // ✅ using localized ajaxurl
+            type: 'POST',
+            data: {
+                action: 'filter_case_studies',
+                category: category
+            },
+            beforeSend: function () {
+                $('#caseStudyResults').html('<p>Loading...</p>');
+            },
+            success: function (response) {
+                $('#caseStudyResults').html(response);
+            }
+        });
+    });
+});
