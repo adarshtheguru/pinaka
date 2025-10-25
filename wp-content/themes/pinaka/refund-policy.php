293551cff6badaec23c2eda6b0f1f91b2aea6608
pragneshui <pragneshlimbasiya@gmail.com>
2025-10-26 00:38:33 +0530

    link updated and images

---
+++ wp-content/themes/pinaka/refund-policy.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Template Name: Refund Policy
+*/
+get_header();
+?>
+<main class="aboutUs privacy-page">
+	<?php include 'customTemplates/heroSection.php' ?>
+	<section class="padding aboutUsOverview pos-rel" id="aboutUsOverview">
+		<div class="container">
+			<div class="secPrefix scroll-animate fade-in">Refund Policy</div>
+			<p>At Pinaka, we strive to deliver exceptional service and high-quality results for all of our clients. To ensure optimal project execution and resource allocation, we maintain a clear policy regarding refunds. Once a project commences, including the initial investment and resource allocation, refunds are not available. However, we are committed to open communication throughout the project lifecycle. We will gladly discuss any concerns you may have and work collaboratively to ensure your complete satisfaction with the final outcome.</p>
+		</div>
+	</section>
+</main>
+<?php include 'customTemplates/footerGreen.php'; ?>
+<?php get_footer(); ?>
