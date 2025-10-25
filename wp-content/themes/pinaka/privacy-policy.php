5ca27a1ebe99acbcb235dce210c2546c9ecc745e
pragneshui <pragneshlimbasiya@gmail.com>
2025-10-25 23:57:07 +0530

    link updated and images

---
+++ wp-content/themes/pinaka/privacy-policy.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Privacy Policy
+ */
+get_header();
+?>
+
+<main class="aboutUs">
+    <?php include 'customTemplates/heroSection.php' ?>
+    <section class="padding aboutUsOverview pos-rel" id="aboutUsOverview">
+        <div class="container">
+            <div class="secPrefix  scroll-animate fade-in">Privacy Policy</div>
+
+        </div>
+    </section>
+</main>
+
+
+<?php include 'customTemplates/footerGreen.php'; ?>
+
+<?php get_footer(); ?>
