9db56f332a2aa2bff71ff15418f35396083948c7
pragneshui <pragneshlimbasiya@gmail.com>
2025-10-26 00:15:15 +0530

    link updated and images

---
+++ wp-content/themes/pinaka/terms-conditions.php
@@ -0,0 +1,58 @@
+<?php
+/**
+* Template Name: Terms & Conditions
+*/
+get_header();
+?>
+<main class="aboutUs privacy-page">
+	<?php include 'customTemplates/heroSection.php' ?>
+	<section class="padding aboutUsOverview pos-rel" id="aboutUsOverview">
+		<div class="container">
+			<div class="secPrefix scroll-animate fade-in">Terms & Conditions</div>
+			<div class="scroll-animate fade-in">
+				<h2>Pinaka Terms of Use </h2>
+				<p>This user agreement is an electronic record created in accordance with the Information Technology Act, 2000 (the “Act”) and any applicable rules thereunder, including amendments related to electronic records as defined in various statutes by the Information Technology (Amendment) Act, 2008. This agreement is generated electronically and does not require physical signatures.</p>
+				<p>Please take the time to carefully review these terms of use. By accessing and using the Pinaka website, you are agreeing to be bound by these terms. We recommend reviewing these terms periodically as your continued use of the website after any changes are posted will be considered your acceptance of those changes. Pinaka reserves the right to modify these terms of use at any time, and any such modifications will be posted on this page.</p>
+				<p>If you do not agree to these terms of use, you are prohibited from using the Pinaka website in any way.</p>
+				<h3>Website Information</h3>
+				<p>Pinaka strives to ensure that all information published on this website is accurate and up-to-date at the time of posting. However, we cannot guarantee its complete accuracy or timeliness and reserve the right to modify information at any time without prior notice. Users are advised not to rely solely on the information presented here and to take appropriate steps to verify its validity before making any decisions based on it.</p>
+				<p>The information contained on this website does not constitute an invitation to engage in any business relationship, invest in shares or securities, purchase products or services, or enter into any contracts with Pinaka or any other company. It is not intended to be a substitute for professional advice when making business decisions. We encourage users to seek professional guidance before entering into any business relationships with Pinaka or its affiliates.</p>
+				<p>References to any products or services offered by Pinaka or other companies do not guarantee their availability at all times. We may make changes or improvements to such products or services without prior notice.</p>
+				<p>By using this website, you acknowledge that you have read and fully understood our Privacy Policy (available on the website) and any other documents, instructions, etc., included here, which become part of these Terms of Use. You further agree that you find the terms and content of the Privacy Policy acceptable. Submitting data to us or using the website constitutes your consent to our use of that data and allows us to communicate with you to facilitate your use of our services.</p>
+				<p>This website may contain links to external websites owned and operated by third parties not affiliated with Pinaka (“Third-Party Websites”). Pinaka has no control over the content or practices of Third-Party Websites.</p>
+				<h3>Pinaka Website Disclaimer</h3>
+				<p>Pinaka provides this website “as is” without warranty of any kind. We make no express or implied representations, warranties, or guarantees about the accuracy, completeness, or timeliness of the content on this website.</p>
+				<p>This disclaimer applies to the fullest extent permissible by law. Pinaka will not be held liable for any death or personal injury arising from our negligence, fraud, or fraudulent misrepresentation. Additionally, we are not liable for any other situation where the law prohibits exclusion or limitation of liability.</p>
+				<p>Pinaka will not be responsible for any loss or damage you experience, regardless of whether it arises from a contractual agreement, tort (including negligence), breach of statutory duty, or any other legal theory. This includes, but is not limited to, situations where the loss or damage arises from:</p>
+				<ul>
+					<li>Using or being unable to use our website.</li>
+					<li>Using or relying on any content displayed on our website.
+					</li>
+				</ul>
+				<h2>For Business Users:</h2>
+				<h3>Pinaka specifically disclaims liability for:</h3>
+				<ul>
+					<li>Loss of profits, sales, business, or revenue.</li>
+					<li>Business interruption.</li>
+					<li>Loss of anticipated savings.</li>
+					<li>Loss of business opportunity, goodwill, or reputation.</li>
+					<li>Any indirect or consequential loss or damage.</li>
+				</ul>
+				<p>Pinaka is not responsible for any loss or damage caused by viruses, distributed denial-of-service attacks, or other harmful material that may infect your device, computer programs, data, or other proprietary material due to your use of our website or downloading content from it or any linked websites.</p>
+				<p>We do not endorse any websites linked to our website and are not responsible for their content or any loss or damage that may arise from your use of them.</p>
+				<p>Pinaka cannot guarantee that the website will always be available or uninterrupted. We reserve the right to suspend access to the website without notice.</p>
+				<h3>Hyperlink:</h3>
+				<p>Pinaka cannot be held responsible for the content of any external websites linked to our website. We make no guarantees or claims about the accuracy or quality of information found on these external sites.</p>
+				<p>Visiting and using any third-party websites is entirely at your own risk. Pinaka will not be involved in any transactions you conduct with these external websites. Your use of these websites is subject to their own terms and conditions, which may differ from ours. In the case of any conflict, these Terms of Use for Pinaka will take precedence.</p>
+				<p>Our website may display advertisements, promotions, and other content from third parties. These may include hyperlinks or buttons that lead to external websites. The presence of such advertising does not constitute an endorsement or recommendation by Pinaka of the advertiser, its products or services, or the linked website itself. For all information regarding the advertiser and its offerings, please refer directly to the advertiser. Pinaka takes no responsibility for any interactions you have with these third parties. We are released from any liability arising out of or connected to such interactions, including any defects, deficiencies, claims, etc. related to the advertiser’s products or services.</p>
+				<p>The Privacy Policy and any other documents, instructions, or information included on our website are considered part of thes Terms of Use and should be read alongside them.</p>
+				<h3>Copyright:</h3>
+				<p>All content on the Pinaka website, including text, graphics, logos, icons, images, audio clips, downloads, data compilations, and software, is the exclusive property of Pinaka and is protected by Indian copyright law. This includes the way all this content is arranged on our website.</p>
+				<p>Using any of Pinaka’s website content without permission is strictly prohibited.</p>
+				<p>If you have any questions or comments regarding our copyright statement, please don’t hesitate to contact us.</p>
+			</div>
+		</div>
+	</section>
+</main>
+<?php include 'customTemplates/footerGreen.php'; ?>
+<?php get_footer(); ?>
