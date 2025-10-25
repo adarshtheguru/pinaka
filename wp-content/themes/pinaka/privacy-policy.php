38ef6d020e9c20479583bf160e70c911009bcd7c
pragneshui <pragneshlimbasiya@gmail.com>
2025-10-26 00:02:02 +0530

    link updated and images

--- wp-content/themes/pinaka/privacy-policy.php
+++ wp-content/themes/pinaka/privacy-policy.php
@@ -1,21 +1,35 @@
 <?php
 /**
- * Template Name: Privacy Policy
- */
+* Template Name: Privacy Policy
+*/
 get_header();
 ?>
-
 <main class="aboutUs">
-    <?php include 'customTemplates/heroSection.php' ?>
-    <section class="padding aboutUsOverview pos-rel" id="aboutUsOverview">
-        <div class="container">
-            <div class="secPrefix  scroll-animate fade-in">Privacy Policy</div>
-
-        </div>
-    </section>
+	<?php include 'customTemplates/heroSection.php' ?>
+	<section class="padding aboutUsOverview pos-rel" id="aboutUsOverview">
+		<div class="container">
+			<div class="secPrefix  scroll-animate fade-in">Privacy Policy</div>
+			<h2>Privacy Policy for Pinaka Digital Marketing Agency</h2>
+			<h3>Information use:</h3>
+			<p>Pinaka Digital Technologies is the sole owner of the information provided and collected on this website. We shall not share, sell or rent this information with/to others in ways other than what is disclosed in this given statement.</p>
+			<h3>Order Information:</h3>
+			<p>Our “enquiry form” pages request personal information from users. The user here is expected to provide contact information like name, telephone number, email etc. This information helps us fulfill customer enquiries and orders.</p>
+			<h3>About Cookies:</h3>
+			<p>The information about the user is stored in the user’s hard drive in the form of a cookie. This information is in no way linked or similar or any personally identifiable information and is encrypted in such a way that it cannot be read by any other website. The cookie simply gets terminated once the user closes their browser. If we happen to use the cookies it is solely for your convenience.</p>
+			<h3>Use of Log files:</h3>
+			<p>IP addresses are used solely by us to analyze trends, administer the site and gather demographic information. The IP addresses are in no way connected or linked to any personal information.</p>
+			<h3>Information Sharing:</h3>
+			<p>Pinaka Digital Technologies will use your information for our own purposes and may also share it with select partners who help manage aspects of our business. We may also disclose your data to relevant regulatory bodies if required by law. Additionally, we reserve the right to use your data for internal training and testing purposes, always in a way that protects your privacy. If you prefer that your data not be used in any of these ways, you can opt out by contacting us at sales@pinaka.digital.</p>
+			<h3>About Links:</h3>
+			<p>You’ll find links to other websites on our Pinaka website. It’s important to understand that these external sites have their own privacy policies, and we at Pinaka are not responsible for their practices. We encourage you to carefully review the privacy statements of any website you visit, as this policy applies solely to information collected by Pinaka Digital Technologies.</p>
+			<h3>Security:</h3>
+			<p>Pinaka prioritizes the security of your information. When you submit sensitive data through our website, we take steps to safeguard it online and offline. User information collected through our website is restricted to our internal systems. Only authorized personnel, such as accounts or customer service representatives, may be granted access to your specific information, and only for necessary tasks related to your inquiry.</p>
+			<h3>Update/Correction of Personal Information:</h3>
+			<p>Pinaka understands that your personal information may change over time.  We offer you the ability to easily access, update, or even remove your personal data from our systems.  If you wish to unsubscribe from receiving communications like newsletters or promotional materials, you can do so by following the unsubscribe instructions included in those emails, or by contacting us directly. We are committed to respecting your preferences.</p>
+			<h3>Notification:</h3>
+			<p>Pinaka reserves the right to update this Privacy Policy from time to time. We will always post any changes on this website to ensure you’re informed about the information we collect, how we use it, and under what circumstances we might disclose it. This transparency allows you to stay up-to-date on our data practices.</p>
+		</div>
+	</section>
 </main>
-
-
 <?php include 'customTemplates/footerGreen.php'; ?>
-
 <?php get_footer(); ?>
